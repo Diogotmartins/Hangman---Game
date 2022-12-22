8739c5bf01402e9af7f9147b8b9570b8455ea5ea
Diogo Travassos Martins <diogotmartins@gmail.com>
2022-12-21 23:19:13 -0300

    Cria a Hangout.py

---
+++ Hangout.py
@@ -0,0 +1,42 @@
+#GAME HANGMAN
+#Atividade do curso DSA
+
+# Import
+
+import random
+
+#Criar tabuleiro usando uma lista
+
+board = ['''
+HANGMAN THE GAME
+
++----+
+|    |
+     |
+     |
+     |
+     |
+===========''' , '''
++----+
+|    |
+O    |
+     |
+     |
+     |
+===========''' , '''
++----+
+|    |
+O    |
+|    |
+     |
+     |
+===========''' , '''
+ +----+
+ |    |
+ O    |
+/|    |
+      |
+      |
+===========''' , 
+
+''']
